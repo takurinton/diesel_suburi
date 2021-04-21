9ada7b4421d71d38a503dca21a37959d42bb5104
takurinton <taryochanmannn@icloud.com>
2021-04-21 22:06:23 +0900

    error: is not satisfied the following implementations were found

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] 
+extern crate rocket;
+
+use diesel::result::Error;
+
+use rocket_contrib::json::Json;
+
+use diesel::*;
+use diesel_sample::*;
+use self::models::*;
+use self::schema::posts::dsl::*;
+
+#[get("/")]
+fn index() -> Result<Json<Vec<Post>>, Error> {
+    let conn = establish_connection();
+    posts.
+    load(&conn)
+    .map(|post| Json(post))
+}
+
+#[get("/about")]
+fn about() -> &'static str {
+    "Hello, about!!!"
+}
+ 
+#[get("/takurinton")]
+fn takurinton() -> &'static str {
+    "Hello, takurinton!"
+}
 
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![
+        index,
+        about,
+        takurinton
+        ]).launch();
 }
