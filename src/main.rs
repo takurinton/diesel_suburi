0bbcbf4ae580823e49341a8b1f5267aa28ad94b9
takurinton <taryochanmannn@icloud.com>
2021-04-22 07:06:54 +0900

    fix: models.rs

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use diesel_sample::*;
 use self::models::*;
 use self::schema::posts::dsl::*;
 
+
 #[get("/")]
 fn index() -> Result<Json<Vec<Post>>, Error> {
     let conn = establish_connection();
@@ -20,20 +21,8 @@ fn index() -> Result<Json<Vec<Post>>, Error> {
     .map(|post| Json(post))
 }
 
-#[get("/about")]
-fn about() -> &'static str {
-    "Hello, about!!!"
-}
- 
-#[get("/takurinton")]
-fn takurinton() -> &'static str {
-    "Hello, takurinton!"
-}
-
 fn main() {
     rocket::ignite().mount("/", routes![
         index,
-        about,
-        takurinton
         ]).launch();
 }
--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,16 @@
+use diesel;
+// use diesel::prelude::*;
+// use super::schema::posts;
+
 use diesel::{Queryable};
-#[derive(Queryable)]
+use serde::{Serialize};
+
+#[derive(Serialize, Queryable)] 
 pub struct Post {
     pub id: i32,
     pub title: String,
     pub body: String,
     pub published: bool,
 }
+
+impl Post {}
